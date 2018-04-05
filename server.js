e9043fcfed006493899cc07988b4c513bbfd5080
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2018-04-04 21:50:23 -0700

    handlebars working on page load
    scraped data populates handlebars on page load
    removed -scrap- button
    added scraped image from post
    zero styling as of now

--- server.js
+++ server.js
@@ -19,9 +19,6 @@ app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
 app.get("/", (req,res) => {
-    res.render("index");
-})
-app.get("/api/scrape", (req, res) => {
     request("https://www.theonion.com/", (error, response, html) => {
         const $ = cheerio.load(html);
         let results = [];
@@ -30,11 +27,13 @@ app.get("/api/scrape", (req, res) => {
             const title = $(element).find("h1").find("a").text();
             const link = $(element).find("h1").find("a").attr("href");
             const summary = $(element).find(".excerpt").find("p").text();
+            const image = $(element).find("picture").find("source").attr("data-srcset");
 
             results.push({
                 title:title,
                 link:link,
                 summary:summary,
+                image:image
             });
             
         })
